106151f0b3f645f1a9fe01f93b93498de90daaf6
Alice Zhang <zhangjingyu.98@gmail.com>
2023-12-17 00:10:32 +0800

    defined left, right and center in the topbar to add socmed icons

--- src/topbar/topbar.jsx
+++ src/topbar/topbar.jsx
@@ -2,5 +2,9 @@
 import "./topbar.css";
 
 export default function TopBar() {
-  return <div className="top">topbar</div>;
+  return <div className="top">
+    <div className="topLeft">1</div>
+    <div className="topCenter">2</div>
+    <div className="topRight">3</div>
+  </div>;
 }
