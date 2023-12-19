c789f12b8a02dbf793331fac9059481e030920e0
Alice Zhang <zhangjingyu.98@gmail.com>
2023-12-19 18:03:22 +0800

    header that says header on my homepage hahaha

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import TopBar from "./topbar/topbar";
+import Home from "./pages/home/home"
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import "./home.css"
+import Header from  "../../header/header"
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Header />
+      homepage
+    </div>
+  );
+}
