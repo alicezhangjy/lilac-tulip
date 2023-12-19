d3bb102f6a5fca80fbe73e09aa4e528576a86d6c
Alice Zhang <zhangjingyu.98@gmail.com>
2023-12-19 18:15:46 +0800

    decorate homepage header with title and background image

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -3,7 +3,11 @@ import "./header.css"
 export default function Header(){
     return(
         <div className="header">
-            header
+            <div className="headerTitles"> 
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleLg">Blog</span>
+            </div>
+            <img className="headerImg" src="https://www.gistyarn.com/cdn/shop/files/Beam_1200x.jpg?v=1649093718"></img>
         </div>
     )
 }
